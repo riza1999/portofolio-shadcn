8f755cdc704a23b7b69080d7d9a5d6740510a879
riza1999 <49582374+riza1999@users.noreply.github.com>
2023-05-09 15:35:07 +0700

    change github link

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Riza Kurniawanda Portofolio",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Riza Kurniawanda Portofolio",
   mainNav: [
     {
       title: "Home",
@@ -11,8 +11,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
+    github: "https://github.com/riza1999",
     docs: "https://ui.shadcn.com",
   },
 }
